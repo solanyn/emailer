2c685c91c8e4cb485f43a57f774f936d277075ae
solanyn <14799876+solanyn@users.noreply.github.com>
2024-11-04 22:10:42 +1100

    fix: use new attachment
    * Use Attachment::new() over new_inline() since the email print service doesn't seem to understand inline attachments well
    * Add more logging

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use lettre::message::{Attachment, Mailbox, MultiPart, SinglePart};
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
 use std::env;
+use url::Url;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Set default logging level to info
@@ -14,25 +15,44 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     env_logger::init();
 
+    log::info!("Parsing config from environment...");
     // Parse config from environment
     let config = Config::builder()
         .add_source(config::Environment::with_prefix("EMAILER").try_parsing(true))
         .build()
         .unwrap();
     let c: EmailerConfig = config.try_deserialize().unwrap();
-    log::info!("Emailer Configuration Values:\n{:?}", c);
+    log::info!("Emailer config: {:#?}", c);
 
     // Download attachment and store in memory
-    let attachment_resp = reqwest::blocking::get(c.email_attachment_url).unwrap();
+    log::info!("Downloading attachment...");
+    let attachment: Vec<u8> = reqwest::blocking::get(&c.email_attachment_url)
+        .unwrap()
+        .bytes()
+        .unwrap()
+        .to_vec();
+
+    let fname = Url::parse(&c.email_attachment_url.to_string())
+        .unwrap()
+        .path()
+        .split("/")
+        .into_iter()
+        .last()
+        .unwrap()
+        .to_string();
+    log::info!("Downloaded {} from {}", fname, c.email_attachment_url);
 
     // Set timezone for subject line
+    log::info!("Getting TZ from environment variable...");
     let tz: chrono_tz::Tz = std::env::var("TZ")
         .unwrap_or(String::from("UTC"))
         .parse()
         .unwrap();
     let now = chrono::Utc::now().with_timezone(&tz);
+    log::info!("Got current datetime {} in timezone {}", now, tz);
 
     // Build email message with attachment
+    log::info!("Building email message...");
     let email = Message::builder()
         .from(Mailbox::new(None, c.from_email.parse().unwrap()))
         .to(Mailbox::new(None, c.to_email.parse().unwrap()))
@@ -44,10 +64,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         .header(ContentType::TEXT_HTML)
                         .body(String::from("")),
                 )
-                .singlepart(Attachment::new_inline(String::from("1")).body(
-                    attachment_resp.bytes().unwrap().to_vec(),
-                    ContentType::parse("image/jpeg").unwrap(),
-                )),
+                .singlepart(
+                    Attachment::new(fname)
+                        .body(attachment, ContentType::parse("image/jpeg").unwrap()),
+                ),
         )
         .unwrap();
 
@@ -55,15 +75,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let creds = Credentials::new(c.smtp_username, c.smtp_password);
 
     // Open a remote connection to SMTP server
+    log::info!("Logging into SMTP server {}...", c.smtp_server);
     let mailer = SmtpTransport::starttls_relay(&c.smtp_server)?
         .credentials(creds)
         .build();
 
     // Send the email
+    log::info!("Sending email...");
     match mailer.send(&email) {
         Ok(_) => log::info!("Email sent successfully!"),
         Err(e) => log::error!("Could not send email: {e:?}"),
     }
 
+    log::info!("Exiting...");
     Ok(())
 }
